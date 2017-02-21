dac7439863b7abe7ec534ea0ed70b1c80fcc353c
Chris Parolini <cparolini@cparolini-mbp1.ip-soft.net>
2017-02-21 15:06:00 -0600

    inital file stucture for burger homework

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, date) VALUES ('Bacon Burger', NOW());
+INSERT INTO burgers (burger_name, date) VALUES ('Cheese Burger', NOW());
+INSERT INTO burgers (burger_name, date) VALUES ('Avacado Burger', NOW());
+INSERT INTO burgers (burger_name, date) VALUES ('Turkey Burger', NOW());
+INSERT INTO burgers (burger_name, date) VALUES ('Plain Burger', NOW());
+INSERT INTO burgers (burger_name, date) VALUES ('Veggie Burger', NOW());
